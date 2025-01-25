7ffed1d3b98d9bd87cc8270050cfafa748515660
Milton Palmeira <lpsantana_@hotmail.com>
2025-01-25 15:46:38 -0300

    feat(event-bus): process event if query is down

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,26 +1,37 @@
-import express from "express";
-import axios from "axios";
+import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/events", (req, res) => {
+const events = [];
+
+app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log('localhost:4000 - ' + err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log('localhost:4001 - ' + err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log('localhost:4002 - ' + err.message);
   });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log('localhost:4003 - ' + err.message);
+  });
 
-  res.send({ status: "OK" });
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log('Listening on 4005');
 });
